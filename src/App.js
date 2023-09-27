64626b36b00731264cba9f0226bed0b4db07096c
hasan <mmhotmail>
2023-09-27 10:57:49 +0300

    about

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import AppRouter from './router/AppRouter';
+import AppRouter from "./router/AppRouter";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darktheme } from "./styles/theme";
+import { GlobalStyles } from "./styles/Global.styles";
+import { useThemeContext } from "./context/ThemeContext";
+import AuthContextProvider from "./context/AuthContext";
+import BooksContextProvider from "./context/BooksContext";
 
 function App() {
+  const { myTheme } = useThemeContext();
+  const themes = myTheme === "light" ? lightTheme : darktheme;
   return (
-    <div className="App">
-      <AppRouter/>
-    </div>
+    <>
+      <ThemeProvider theme={themes}>
+        <GlobalStyles />
+        <AuthContextProvider>
+          <BooksContextProvider>
+            <AppRouter />
+          </BooksContextProvider>
+        </AuthContextProvider>
+      </ThemeProvider>
+    </>
   );
 }
 
